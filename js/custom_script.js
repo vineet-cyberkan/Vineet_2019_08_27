21168619c4fa29838027fb5d24354a1392f833b3
vineet-cyberkan <vineet1.st@gmail.com>
2019-08-27 15:19:00 +0530

    created 'canvas' & 'select' html dynamically and added to html

--- js/custom_script.js
+++ js/custom_script.js
@@ -26,12 +26,12 @@ var d_Cont = '#dynamic_content', photos_Data = [];
 	            contentType: 'application/json',
 	            dataType: 'json',
 
-	            success: function success(photosData) {
+	            success: function(photosData) {
 					//console.log( "photosData =>"+photosData);
 					//console.log( "photosData count =>"+photosData.length);
 					if (photosData.length > 0 ) {
 						photos_Data = photosData;    // stored data into an object when genrating canvas again and again
-						canvas.generate.createDynamicButtons(); // to run in sequence 
+						//canvas.generate.createDynamicButtons(); // to run in sequence 
 					}
 	            },
 
@@ -41,11 +41,57 @@ var d_Cont = '#dynamic_content', photos_Data = [];
         	});
 		},
 
+		generate_uniq_int: function(min_num, max_num){
+			min_num = Math.ceil(min_num);
+		    max_num = Math.floor(max_num);
+		    return Math.floor(Math.random() * (max_num - min_num + 1)) + min_num;
+		},
+
 		createDynamicButtons: function(){
 			if( photos_Data.length > 0 ){
+				var dynamic_select = $('<select id="select" class="form-control"><option value="0">Canvas #</option></select>'),
+				    btn_Insert = '<a href="javascript:void(0)" class="btn btn-primary" id="insert_canvas">Insert to Canvas</a>',
+				    photosDataNode,
+				    canvas_area = $('<div id="canvas_area" />'),
+				    uniq_int,
+				    canvas_space = 20,
+				    canvas_stroke = 1,
+				    canvas_attr;
+
+				$(d_Cont).empty().append(dynamic_select, btn_Insert, '<hr />', canvas_area);
+				uniq_int = canvas.generate.generate_uniq_int(2, 4);
+
+				console.log("uniq_int =>"+uniq_int);
+				canvas_attr = parseInt( ( $(d_Cont).width()/2 ) - ( (canvas_stroke+canvas_space)*2 ) ); // calculate for canvas styling
+				console.log("main container width =>"+canvas_attr);
+
+				// append dynamic values
+
+				for (var number = 0; number < uniq_int; number++) {
+					dynamic_select.append('<option id="customSelectOption'+(number+1)+'" value="'+(number+1)+'">Canvas '+(number+1)+'</option>');
+					//canvas_area.append('<div class="canvasbox" style="margin:'+canvas_space+'px;"><canvas height="'+canvas_attr+'" width="'+canvas_attr+'" id="canvasbox'+(number+1)+'"></canvas></div>');
+					canvas_area.append(
+						'<canvas height="'+canvas_attr
+						+'" width="'+canvas_attr
+						+'" id="canvasbox'+(number+1)
+						+'" style="border:'+canvas_stroke+'px solid #000;'
+							+'margin:'+canvas_space+'px;'
+						+'"></canvas>'
+						);
+				}				
 
 			} else {
-				$('#dynamic_content').html('<p>No data avialable </p>')
+				$('#dynamic_content').html('<p>No data avialable</p>')
+			}
+		},
+
+		insertToCanvas: function(){
+			var canvas_value = $('#select').val();
+			if ( canvas_value != 0 ) {
+				//$('#canvasbox'+canvas_value).parent().css('border-color','red' )
+				$('#canvasbox'+canvas_value).css('border-color','#f00' )
+			} else {
+				alert('Please select a canvas!')
 			}
 		}
 	}
@@ -55,6 +101,14 @@ window.onload = function(){
 	if ($(d_Cont).length) {
 		canvas.generate.init();   // data request sent on page load
 	}
+
+	$(document).on('click', '#genrate_canvas', function() {
+		canvas.generate.createDynamicButtons();
+	});
+
+	$(document).on('click', '#insert_canvas', function() {
+		canvas.generate.insertToCanvas();
+	});
 }
 
 window.onresize = function(){
